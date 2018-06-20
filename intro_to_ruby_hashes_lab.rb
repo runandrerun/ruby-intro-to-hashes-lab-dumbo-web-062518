ab5e8b72269864b687a2f95761818608bfd370a3
Andre Santiago <the.asantiagojr@gmail.com>
2018-06-20 15:09:06 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -32,15 +32,8 @@ def monopoly_with_third_tier
 	  :railroads => {
 	    :pieces => 4,
 	    :names => {
-	      :reading_railroad => 
-	      {
-	        :mortgage_value => "$100"
-	        
-	      },
-	      :pennsylvania_railroad => 
-	      { 
-	        :mortgage_value => "$200" 
-	      },
+	      :reading_railroad => { "mortgage_value" => "$100" },
+	      :pennsylvania_railroad => { "mortgage_value" => "$200" },
 	      :b_and_o_railroad => {},
 	      :shortline => {}
 	    },
